74d6f68afa37a4221cdfe0a0bb19c0578b9ad05e
Taraxacum0112 <yura.krivoruchko.19990112@gmail.com>
2024-09-30 19:30:04 +1000

    Настройка соединения
    Налажена связь между сервером и клиентом

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from client_ui import Ui_MainWindow
+import socket
 
 class ClientWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +9,31 @@ class ClientWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+def connect (): # по нажатию кнопки подключиться
+    full_text = window.ui.line_ip.text()
+    ip = full_text.replace('IP: ', '').strip()
+    port = 10000
+    if (ip_is_valid(ip) and port != ""):
+        socket.connect((ip, int(port)))
+    else:
+        print(f"IP адресс должен быть в формате xxx.xxx.xxx.xxx")
+        #надо бы добавить выскакивающее окно ошибки
+        #error_ip.config(text="IP адресс должен быть в формате xxx.xxx.xxx.xxx")
+
+def ip_is_valid (ip):
+    try:
+        host_bytes = ip.split('.')
+        valid = [int(b) for b in host_bytes]
+        valid = [b for b in valid if b>=0 and b<=255]
+        return len(host_bytes) == 4 and len(valid) == 4
+    except:
+        return False  
+    
+def send_message (): #по нажатию кнопки отправить
+    data = window.ui.line_message.text()
+    socket.sendall(data.encode())
+    
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from server_ui import Ui_MainWindow
+import socket
 
 class ServerWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +9,26 @@ class ServerWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+def start_server():
+    HOST = (ip, port)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(HOST)
+
+    server.listen()
+    print('Start listening')
+
+    connection, address = server.accept()
+    print(f'New connection from {address}')
+    
+    while 1:
+        data = connection.recv(1024).decode()
+        print(data) # поменять на вывод в окно сообщений
+    
+    connection.close()
+    server.close()
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 10000    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
