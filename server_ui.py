e935e8eabedceb191878f914d630f84df1cf3a63
Nania01 <148682556+Nania01@users.noreply.github.com>
2024-10-03 15:22:18 +1000

    Изменён дизайн (добавлены кнопки)

--- server_ui.py
+++ server_ui.py
@@ -17,6 +17,11 @@ class Ui_MainWindow(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.label_ip = QtWidgets.QLabel(self.layoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_ip.sizePolicy().hasHeightForWidth())
+        self.label_ip.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("Bahnschrift SemiCondensed")
         font.setPointSize(12)
@@ -31,6 +36,11 @@ class Ui_MainWindow(object):
         self.label_ip.setObjectName("label_ip")
         self.horizontalLayout.addWidget(self.label_ip)
         self.label_status = QtWidgets.QLabel(self.layoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_status.sizePolicy().hasHeightForWidth())
+        self.label_status.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("Bahnschrift SemiCondensed")
         font.setPointSize(12)
@@ -52,22 +62,22 @@ class Ui_MainWindow(object):
         font.setWeight(75)
         self.button_run.setFont(font)
         self.button_run.setStyleSheet("QPushButton {\n"
-"    background-color: rgb(227, 24, 55);       /* Красный фон в обычном состоянии */\n"
-"    border-radius: 10px;                      /* Закругленные углы */\n"
-"    padding: 10px;                            /* Внутренние отступы */\n"
-"    color: rgb(255, 255, 255);                /* Белый текст */\n"
-"    border: 2px solid rgb(227, 24, 55);       /* Красная рамка */\n"
-"    font-weight: bold;                        /* Жирный текст */\n"
+"    background-color: rgb(227, 24, 55);   \n"
+"    border-radius: 10px;                \n"
+"    padding: 10px;                          \n"
+"    color: rgb(255, 255, 255);             \n"
+"    border: 2px solid rgb(227, 24, 55);       \n"
+"    font-weight: bold;                    \n"
 "}\n"
 "\n"
 "QPushButton:hover {\n"
-"    background-color: rgb(200, 20, 50);       /* Темно-красный фон при наведении */\n"
-"    color: rgb(255, 255, 255);                /* Белый текст */\n"
+"    background-color: rgb(200, 20, 50);     \n"
+"    color: rgb(255, 255, 255);        \n"
 "}\n"
 "\n"
 "QPushButton:pressed {\n"
-"    background-color: rgb(180, 20, 45);       /* Более темный красный при нажатии */\n"
-"    color: rgb(255, 255, 255);                /* Белый текст */\n"
+"    background-color: rgb(180, 20, 45);      \n"
+"    color: rgb(255, 255, 255);              \n"
 "}\n"
 "")
         self.button_run.setObjectName("button_run")
@@ -106,7 +116,73 @@ class Ui_MainWindow(object):
 "")
         self.line_message.setObjectName("line_message")
         self.horizontalLayout_2.addWidget(self.line_message)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.button_clip = QtWidgets.QPushButton(self.layoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_clip.sizePolicy().hasHeightForWidth())
+        self.button_clip.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift SemiBold")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.button_clip.setFont(font)
+        self.button_clip.setStyleSheet("QPushButton {\n"
+"    background-color: rgb(255, 255, 255); \n"
+"    border-radius: 15px;                 \n"
+"    padding: 10px;                        \n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(230, 230, 230);\n"
+"}\n"
+"\n"
+"")
+        self.button_clip.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("image/icon_clip.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button_clip.setIcon(icon)
+        self.button_clip.setIconSize(QtCore.QSize(25, 25))
+        self.button_clip.setObjectName("button_clip")
+        self.horizontalLayout_3.addWidget(self.button_clip)
+        self.button_smiley = QtWidgets.QPushButton(self.layoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_smiley.sizePolicy().hasHeightForWidth())
+        self.button_smiley.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift SemiBold")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.button_smiley.setFont(font)
+        self.button_smiley.setStyleSheet("QPushButton {\n"
+"    background-color: rgb(255, 255, 255); \n"
+"    border-radius: 15px;                 \n"
+"    padding: 10px;                        \n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(230, 230, 230);\n"
+"}\n"
+"")
+        self.button_smiley.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("image/icon_smiley.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button_smiley.setIcon(icon1)
+        self.button_smiley.setIconSize(QtCore.QSize(25, 25))
+        self.button_smiley.setObjectName("button_smiley")
+        self.horizontalLayout_3.addWidget(self.button_smiley)
         self.button_send = QtWidgets.QPushButton(self.layoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_send.sizePolicy().hasHeightForWidth())
+        self.button_send.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("Bahnschrift SemiBold")
         font.setPointSize(10)
@@ -117,15 +193,22 @@ class Ui_MainWindow(object):
 "    background-color: rgb(255, 255, 255); \n"
 "    border-radius: 15px;                 \n"
 "    padding: 10px;                        \n"
-"    color: rgb(0, 150, 214);            \n"
 "}\n"
 "\n"
 "QPushButton:hover {\n"
-"    color: rgb(227, 24, 55);           \n"
+"    background-color: rgb(230, 230, 230);\n"
 "}\n"
+"\n"
+"\n"
 "")
+        self.button_send.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("image/icon_send.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button_send.setIcon(icon2)
+        self.button_send.setIconSize(QtCore.QSize(25, 25))
         self.button_send.setObjectName("button_send")
-        self.horizontalLayout_2.addWidget(self.button_send)
+        self.horizontalLayout_3.addWidget(self.button_send)
+        self.horizontalLayout_2.addLayout(self.horizontalLayout_3)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.verticalLayout_2.addLayout(self.verticalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -139,7 +222,6 @@ class Ui_MainWindow(object):
         self.label_ip.setText(_translate("MainWindow", "IP:"))
         self.label_status.setText(_translate("MainWindow", "Статус: не подключён"))
         self.button_run.setText(_translate("MainWindow", "Запустить"))
-        self.button_send.setText(_translate("MainWindow", "Отправить"))
 
 
 if __name__ == "__main__":
